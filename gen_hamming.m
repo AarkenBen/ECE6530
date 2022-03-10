9109bad9ef07abd6fd7fd9eb7efc377583cc562e
Nathan <u0638554@umail.utah.edu>
2022-03-10 13:38:30 -0700

    add comments to p4_2 and function

--- gen_hamming.m
+++ gen_hamming.m
@@ -2,7 +2,10 @@ function h = gen_hamming(wc,L)
 %gen_h generate filter coefficients
 h = zeros(1,L);
 for n = 0:L-1
+    % calculate the hamming window
     h(n+1) = (0.54 - 0.46 * cos(2*pi*n/(L-1)))*cos(wc *(n - (L - 1)/2));
 end
+% scale the hamming window to give a magnitude of 1 in the pass band
+h = h ./ sum(abs(h)) / 0.828;
 end
 
