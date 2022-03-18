ca33fc41c5b75b9cb2290f606428117d4869a557
Nathan <u0638554@umail.utah.edu>
2022-03-18 07:35:43 -0600

    fix x(t) with 0s in between segments according to instructor comments on piazza

--- p5.m
+++ p5.m
@@ -52,13 +52,21 @@ for i = 1:round(0.25*8000)
     % formula for first segment
     xx(i) = cos(2*pi*220*i / 8000);
 end
-% second segment goes from 0.25 to 0.55
-for i = round(0.25*8000) + 1:round(0.55*8000)
+% zero in between
+for i = round(0.25*8000) + 1:round(0.30*8000)
+    xx(i) = 0;
+end
+% second segment goes from 0.30 to 0.55
+for i = round(0.30*8000) + 1:round(0.55*8000)
     % formula for second segment
     xx(i) = cos(2*pi*880*i / 8000);
 end
-% third segment goes from 0.55 to 0.85
-for i = round(0.55*8000) + 1:round(0.85*8000)
+% zero in between
+for i = round(0.55*8000) + 1:round(0.60*8000)
+    xx(i) = 0;
+end
+% third segment goes from 0.60 to 0.85
+for i = round(0.60*8000) + 1:round(0.85*8000)
     % formula for third segment
     xx(i) = cos(2*pi*440*i / 8000) + cos(2*pi*1760*i / 8000);
 end
